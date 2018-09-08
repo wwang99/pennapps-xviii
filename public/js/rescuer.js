e44c97be2631c17ef5417d340189333c8559b371
fireteam99 <raymancheungsy@gmail.com>
2018-09-08 16:33:31 -0400

    styling done for rescuer view

---
+++ public/js/rescuer.js
@@ -0,0 +1,11 @@
+$("document").ready(function() {
+    $(".victims > .card").click(function() {
+        const victimID = $(this).attr("id").replace(/\D/g,'');
+        $(".victims").css("display", "none");
+        $("#e"+victimID).css("display", "block");
+    });
+    $("button.btn-dismiss").on("click", function() {
+        $(".expanded").css("display", "none");
+        $(".victims").css("display", "block");
+    })
+});

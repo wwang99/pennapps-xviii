c8c856b2727856eb91e27c44fcf4ba048c4b17ec
William Wang <wow7@cornell.edu>
2018-09-08 10:48:34 -0400

    skeleton

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const express = require("express");
+const _ = require("underscore");
+const bodyParser = require('body-parser');
+
+const MongoClient = require("mongodb").MongoClient;
+
+const app = express();
+
+// EJS templating engine setup
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//MongoDB admin user pw: dtDs3VgQayKd7JKZ
+var uri = "mongodb+srv://admin:dtDs3VgQayKd7JKZ@pennapps-piuu1.gcp.mongodb.net/saveme?retryWrites=true";
+MongoClient.connect(uri, function(err, client) {
+   const collection = client.db("saveme").collection("victims");
+   console.log("connected to mongodb")
+   // perform actions on the collection object
+   // client.close();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> {
+  console.log('Server running on port ' + PORT);
+})

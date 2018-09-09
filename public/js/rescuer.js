0bddae72b33e5860c875489d88c2085b9af2e200
fireteam99 <raymancheungsy@gmail.com>
2018-09-09 00:18:49 -0400

    fixed distances

--- public/js/rescuer.js
+++ public/js/rescuer.js
@@ -15,33 +15,33 @@ socket.on('newVictim', data => {
     console.log(data);
     $(".victims").append(
         `<div class="card" id="c${data.id}" onclick="openCard(${data.id})">
-            <div class="info-wrapper">
-                <h4 class="card-header-text">${data.name}</h4>
-                <div class="details body-text">
-                    <div class="info-block">Distance: ${data.location.coordinates[0]} ${data.location.coordinates[1]}</div> <div class="info-block">Severity: ${data.emergencyLevel}</div>
-                </div>
-            </div>
-        </div>`
+<div class="info-wrapper">
+<h4 class="card-header-text">${data.name}</h4>
+<div class="details body-text">
+<div class="info-block">Distance: ${data.location.coordinates[0]} ${data.location.coordinates[1]}</div> <div class="info-block">Severity: ${data.emergencyLevel}</div>
+</div>
+</div>
+</div>`
     );
     $("body").append(
         `<div class="card expanded" id="e${data.id}">
-            <button class="btn-dismiss" onclick="dismiss()"><i class="fas fa-times"></i></button>
-            <div class="info-container">
-                <div class="expanded-card-header">
-                    <h4 class="card-header-text">${data.name}</h4>
-                </div>
-                <div class="info-wrapper">
-                    <p class="body-text">Phone #: ${data.phone}</p>
-                    <p class="body-text">Distance: ${data.location.coordinates[0]} ${data.location.coordinates[1]}</p>
-                    <p class="body-text">Severity: ${data.emergencyLevel}</p>
-                    <p class="body-text">Description: ${data.info}</p>
-                    <p class="body-text">Number of People: ${data.numPeople}</p>
-                </div>
-                <div class="button-wrapper">
-                    <button class="btn">Rescue</button>
-                </div>
-            </div>
-        </div>`
+<button class="btn-dismiss" onclick="dismiss()"><i class="fas fa-times"></i></button>
+<div class="info-container">
+<div class="expanded-card-header">
+<h4 class="card-header-text">${data.name}</h4>
+</div>
+<div class="info-wrapper">
+<p class="body-text">Phone #: ${data.phone}</p>
+<p class="body-text">Distance: ${data.location.coordinates[0]} ${data.location.coordinates[1]}</p>
+<p class="body-text">Severity: ${data.emergencyLevel}</p>
+<p class="body-text">Description: ${data.info}</p>
+<p class="body-text">Number of People: ${data.numPeople}</p>
+</div>
+<div class="button-wrapper">
+<button class="btn">Rescue</button>
+</div>
+</div>
+</div>`
     )
 })
 
@@ -73,6 +73,11 @@ function initMap() {
                 title: 'My location.'
             });
 
+            marker.addListener('click', function() {
+                map.setZoom(15);
+                map.setCenter(marker.getPosition());
+            });
+
             infoWindow.setPosition(pos);
             infoWindow.setContent('Your location is marked in blue.');
             infoWindow.open(map);
@@ -84,6 +89,23 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+    // load victims from the database and add them to map
+    $.getJSON('/getVictims', function(data) {
+        console.warn(data);
+        for (victim of data) {
+            console.warn(victim.location.coordinates[1]);
+            console.warn(victim.location.coordinates[0]);
+            marker = new google.maps.Marker({
+                position: {
+                    lat: victim.location.coordinates[1],
+                    lng: victim.location.coordinates[0]
+                },
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: 'VICTIM_ID'
+            });
+        }
+    });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
